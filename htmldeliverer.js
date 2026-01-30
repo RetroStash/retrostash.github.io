692b4cd8009cfcbfe8f6ef46b72ccb120e33eac5
RetroStash <TheRetroStash@outlook.com>
2026-01-30 18:40:32 -0500

    Update htmldeliverer.js

--- htmldeliverer.js
+++ htmldeliverer.js
@@ -1,7 +1,7 @@
-fetch('https://cdn.jsdelivr.net/gh/RetroStash/rsSINGLEFILE@35158a775a60198788b3189a1a4af8cad328c8e7/index.html')
-  .then(response => response.text())
-  .then(data => {
+fetch("https://cdn.jsdelivr.net/gh/RetroStash/rsSINGLEFILE@35158a775a60198788b3189a1a4af8cad328c8e7/index.html")
+  .then(r => r.text())
+  .then(html => {
     document.open();
-    document.write(data);
+    document.write(html);
     document.close();
   });
