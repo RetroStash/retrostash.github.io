56e31d069c90567f7297ea004f4cfe42f3c7a849
RetroStash <TheRetroStash@outlook.com>
2026-01-30 17:34:55 -0500

    Update and rename index.html to htmldeliverer.js

---
+++ htmldeliverer.js
@@ -0,0 +1,5 @@
+fetch('https://cdn.jsdelivr.net/gh/RetroStash/rsSINGLEFILE@35158a775a60198788b3189a1a4af8cad328c8e7/index.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('myElement').innerHTML = data;
+  });
