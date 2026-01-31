13c6e2b5f4949408cb0c4b987d1bfdee9e533a59
RetroStash <TheRetroStash@outlook.com>
2026-01-31 09:25:07 -0500

    Update htmldeliverer.js

--- htmldeliverer.js
+++ htmldeliverer.js
@@ -2,11 +2,6 @@ fetch("https://api.github.com/repos/RetroStash/rsSINGLEFILE/commits")
   .then(res => res.json())
   .then(data => {
     const fullSHA = data[0].sha;
-    fetch("https://cdn.jsdelivr.net/gh/RetroStash/rsSINGLEFILE@${fullSHA}/index.html")
-  .then(r => r.text())
-  .then(html => {
-    document.open();
-    document.write(html);
-    document.close();
+    window.location.replace("https://cdn.jsdelivr.net/gh/RetroStash/rsSINGLEFILE@${fullSHA}/index.html");
   })
   .catch(err => console.error(err));
